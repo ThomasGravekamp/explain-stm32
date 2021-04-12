7d698042aec02c14bacf3281131dca19bfefe16a
Thomas Gravekamp <thomas.gravekamp@fontoxml.com>
2021-04-12 13:28:47 +0200

    Add README and current state of code

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Explain STM32
 
-CLI utility which, given any STM32 device name, returns the device's properties.
+Rust learning project. CLI tool which explains STM32 device names.
+
+Example:
+
+STM32F103C8T6
+
+Family: STM32
+Series: F1 (preformance line)
+Sub-family: 103
+Pin count: C (48-pins)
+Flash memory size: 8 (64kB)
+Package: T (LQFP)
+Temperature range: 6 (Industrial, -40 to 85)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,106 @@
+use std::io;
+
+// Allowed values: "STM32"
+fn get_device_family(name: &mut String) -> Option<String> {
+    let expected_string = String::from("STM32");
+    let expected_chars = expected_string.chars();
+    let actual_chars = name.chars();
+
+    let iterator = expected_chars.zip(actual_chars);
+
+    for it in iterator {
+        let (ia, ib) = it;
+
+        if ia != ib {
+            return Option::None;
+        }
+    }
+
+    // Hard coded because the name is always expected to start with STM32
+    name.replace_range(0..5, "");
+
+    Option::Some(String::from("STM32"))
+}
+
+fn display_device_family(family: &Option<String>) {
+    match family {
+        Some(family) => println!("Family: {}", family),
+        None => println!("Unknown device family.")
+    }
+}
+
+enum Series {
+    F0,
+    F1,
+    F2,
+    F3,
+    F4,
+    F7
+}
+
+// Allowed values: "F0", "F1", "F2", "F3", "F4", "F7"
+fn get_device_series(name: &mut String) -> Option<Series> {
+    let series: String = name.chars().take(2).collect();
+
+    if series.eq("F0") {
+        return Option::Some(Series::F0)
+    }
+    else if series.eq("F1") {
+        return Option::Some(Series::F1)
+    }
+    else if series.eq("F2") {
+        return Option::Some(Series::F2)
+    }
+    else if series.eq("F3") {
+        return Option::Some(Series::F3)
+    }
+    else if series.eq("F4") {
+        return Option::Some(Series::F4)
+    }
+    else if series.eq("F7") {
+        return Option::Some(Series::F7)
+    }
+
+    Option::None
+}
+
+fn display_device_series(series: Series) {
+    match series {
+        Series::F0 => println!("Series: F0"),
+        Series::F1 => println!("Series: F1"),
+        Series::F2 => println!("Series: F2"),
+        Series::F3 => println!("Series: F3"),
+        Series::F4 => println!("Series: F4"),
+        Series::F7 => println!("Series: F7")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a valid STM32 device name:");
+
+    // name will be consumed by the functions it is passed to
+    let mut name = String::new();
+
+    // Read user input
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line.");
+
+    // Find device family
+    let family = get_device_family(&mut name);
+    display_device_family(&family);
+
+    if family.is_none() {
+        return;
+    }
+
+    let series = get_device_series(&mut name);
+    if let Some(series) = series {
+        display_device_series(series);
+    }
+    else {
+        return;
+    }
+
+    println!("Debug: {}", name);
 }
