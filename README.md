753ffed1e745665edf18c1b8d0b06f3689106336
Thomas Gravekamp <thomas.gravekamp@fontoxml.com>
2021-04-13 09:47:16 +0200

    Add first unit test

--- README.md
+++ README.md
@@ -13,3 +13,34 @@ Pin count: C (48-pins)
 Flash memory size: 8 (64kB)
 Package: T (LQFP)
 Temperature range: 6 (Industrial, -40 to 85)
+
+## F0 devices
+
+ - _Name (RAM, FLASH, package)_
+ - STM32F030F4 (4kB, 16kB, 20-pin TSSOP)
+ - STM32F030K6 (4kB, 32kB, 32-pin LQFP)
+ - STM32F030C6 (4kB, 32kB, 48-pin LQFP)
+ - STM32F070F6 (6kB, 32kB, 20-pin TSSOP) - With USB interface
+ - STM32F070C6 (6kB, 32kB, 48-pin LQFP) - With USB interface
+ - STM32F030C8 (8kB, 64kB, 48-pin LQFP)
+ - STM32F030R8 (8kB, 64kB, 64-pin LQFP)
+ - STM32F070CB (16kB, 128kB, 48-pin LQFP) - With USB interface
+ - STM32F070RB (16kB, 128kB, 64-pin LQFP) - With USB interface
+ - STM32F030CC (32kB, 256kB, 48-pin LQFP)
+ - STM32F030RC (32kB, 256kB, 64-pin LQFP)
+
+Package codes:
+
+ - F = 20-pin TSSOP
+ - K = 32-pin LQFP
+ - C = 48-pin LQFP
+ - R = 64-pin LQFP
+
+Memory size codes:
+
+ - 4 = 4kB, 16kB
+ - 6 = 4kB, 32kB
+ - 6 = 6kB, 32kB (on 070)
+ - 8 = 8kB, 64kB
+ - B = 16kB, 128kB
+ - C = 32kB, 256kB
--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ fn display_device_family(family: &Option<String>) {
     }
 }
 
+#[derive(Debug, PartialEq)]
 enum Series {
     F0,
     F1,
@@ -40,8 +41,13 @@ enum Series {
 
 // Allowed values: "F0", "F1", "F2", "F3", "F4", "F7"
 fn get_device_series(name: &mut String) -> Option<Series> {
+    // Get the first two characters from the name string
     let series: String = name.chars().take(2).collect();
 
+    // Remove first char from name string
+    name.replace_range(0..1, "");
+
+    // Match on the first two characters and return the corresponding Series value
     match series.as_str() {
         "F0" => Option::Some(Series::F0),
         "F1" => Option::Some(Series::F1),
@@ -64,6 +70,30 @@ fn display_device_series(series: Series) {
     }
 }
 
+enum SubFamily {
+    F030,
+    F070,
+}
+
+fn get_device_sub_family(name: &mut String) -> Option<SubFamily> {
+    let sub_family: String = name.chars().take(3).collect();
+
+    name.replace_range(0..4, "");
+
+    match sub_family.as_str() {
+        "030" => Option::Some(SubFamily::F030),
+        "070" => Option::Some(SubFamily::F070),
+        _ => Option::None
+    }
+}
+
+fn display_device_sub_family(sub_family: SubFamily) {
+    match sub_family {
+        SubFamily::F030 => println!("Sub-family: 030"),
+        SubFamily::F070 => println!("Sub-family: 070"),
+    }
+}
+
 fn main() {
     println!("Enter a valid STM32 device name:");
 
@@ -75,7 +105,7 @@ fn main() {
         .read_line(&mut name)
         .expect("Failed to read line.");
 
-    // Find device family
+    // Find and display device family
     let family = get_device_family(&mut name);
     display_device_family(&family);
 
@@ -83,6 +113,7 @@ fn main() {
         return;
     }
 
+    // Find and display device series
     let series = get_device_series(&mut name);
     if let Some(series) = series {
         display_device_series(series);
@@ -91,5 +122,25 @@ fn main() {
         return;
     }
 
+    let sub_family = get_device_sub_family(&mut name);
+    if let Some(sub_family) = sub_family {
+        display_device_sub_family(sub_family);
+    }
+    else {
+        return;
+    }
+
     println!("Debug: {}", name);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut test_name = String::from("F103C8T6");
+
+        assert_eq!(get_device_series(&mut test_name).unwrap(), Series::F1);
+    }
 }
